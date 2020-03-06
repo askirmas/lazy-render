56b433c2203fe3c7ebc319bd4860527fb17c920e
askirmas <kirmas@phystech.edu>
2020-03-07 00:42:47 +0200

    Add basic test

---
+++ specs/index.spec.ts
@@ -0,0 +1,35 @@
+import puppeteer, { Browser, Page } from 'puppeteer'
+
+let browser: Browser
+, page: Page
+
+beforeAll(async done => {
+  browser = await puppeteer.launch({
+    headless: true
+  })
+  page = (await browser.pages())[0];
+  return done()
+})
+afterAll(() => browser.close())
+
+describe('index', () => {
+  beforeAll(async done => {
+    await page.goto(`file://${process.cwd()}/out/index.html`, {waitUntil: "domcontentloaded"})
+    return done()
+  })
+  it('no children', () => expect(
+      page.$$eval('.child', ({length}) => length)
+  ).resolves.toBe(0))
+  it('check', async () => {
+    const input = await page.$('input')
+    if (!input)
+      return expect(input).not.toBe(null)
+    return expect(input.click()).resolves.toBe(undefined)
+  })
+  it('no children', () => expect(
+    page.waitForSelector('.child')
+    .then(() =>
+      page.$$eval('.child', ({length}) => length)
+    )
+  ).resolves.toBe(2))
+})
